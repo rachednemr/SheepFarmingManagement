18c7ebba7eb587f2409d9b627d0dd78f1568c782
=rachednemr <=rached.nemr@esprit.tn>
2013-10-18 01:00:52 +0100

    premiere commit initiale

---
+++ ejbModule/SheepFarmingManagement/persistance/entities/user.java
@@ -0,0 +1,13 @@
+package SheepFarmingManagement.persistance.entities;
+
+public class user {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ejbModule/SheepFarmingManagement/persistance/gestion/interfaces.java
@@ -0,0 +1,13 @@
+package SheepFarmingManagement.persistance.gestion;
+
+public class interfaces {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
